82c7a73b6dc41bb1a0468b36ba50317dd9b7d5c9
JonathanKless <jonathankless@hotmail.com>
2021-11-04 23:31:34 +0100

    Create outline.py

---
+++ outline.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 15:34:21 2021
+
+@author: Jonat
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# plots / stylings
+plt.xlabel()
+plt.ylabel()
+plt.legend() #no arguments 
+plt.title()
+plt.grid(True)
+plt.styles.use()
+plt.tight_layout()
+plt.savefig("C:\Path\")
+
+#Format strings
+#https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+#use hex values for color color="#4444"
+#use linewidth= 
+
+# plt.plot(same x axis, use label= argument)
+
+
+
