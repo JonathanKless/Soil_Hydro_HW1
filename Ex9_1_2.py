0569e0c3f9d5493459efbd28d6be2588023666e0
JonathanKless <jonathankless@hotmail.com>
2021-11-05 22:08:50 +0100

    Finished (hopefully)

--- Ex9_1_2.py
+++ Ex9_1_2.py
@@ -28,9 +28,18 @@ plt.grid()
 plt.show()
 
 #saving the plot to current directory
-plt.savefig(fname="Exercise_1.png", dpi=200, format="png", figsize=(20,20))
+#plt.savefig(fname="Exercise_1.png", dpi=200, format="png", figsize=(20,20))
 
 """
 Exercise 9.2
 """
 
+#plotting the values in a better visualisation
+plt.figure("Exercise 1")
+plt.semilogy(theta, np.absolute(h), label="Measured Values")
+plt.xlabel("SWC [cm3/cm3]", **csfont)
+plt.ylabel("Matric potential [cm]", **csfont)
+plt.title("Exercise 1", **csfont)
+plt.legend(loc="best", fancybox=True)
+plt.grid()
+plt.show()
---
+++ Ex9_3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 22:07:29 2021
+
+@author: Jonat
+"""
+
